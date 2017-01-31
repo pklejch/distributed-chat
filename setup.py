9061eab59110d3b5555402dad196f7f05c899c90
Petr Klejch <klejcpet@fit.cvut.cz>
2017-01-31 17:51:06 +0100

    Small updates.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as f:
  
 setup(
     name='distributedchat',
-    version='0.9',
+    version='0.9.1',
     description='.',
     long_description=long_description,
     author='Petr Klejch',
